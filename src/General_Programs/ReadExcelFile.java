66cce710cfa8dbd0deb960bb81b1b2e000b03b89
SpSaqib <saqibpendari@gmail.com>
2023-06-15 23:50:32 +0530

    Commit Message

---
+++ src/General_Programs/ReadExcelFile.java
@@ -0,0 +1,18 @@
+package General_Programs;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+public class ReadExcelFile {
+
+	public static void main(String[] args) throws FileNotFoundException {
+		// TODO Auto-generated method stub
+		
+		File f=new File("‪C:\\Users\\HP\\Downloads\\Book1.xlsx");
+		FileInputStream file=new FileInputStream(f);
+
+		Workbookfa
+	}
+
+}
