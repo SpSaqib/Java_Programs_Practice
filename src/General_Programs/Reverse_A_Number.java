1a71e9430074fd25c14988ed3492fb15de23c855
SpSaqib <saqibpendari@gmail.com>
2023-07-10 23:21:12 +0530

    reverse a number updated

--- src/General_Programs/Reverse_A_Number.java
+++ src/General_Programs/Reverse_A_Number.java
@@ -12,23 +12,27 @@ public class Reverse_A_Number {
 		
 		int reverseNumber=0;
 		
-		while(givenNumber!=0)
-		{
-			int reminder=givenNumber%10;
-			reverseNumber=reverseNumber*10+reminder;
-			givenNumber=givenNumber/10;
-		}
-		System.out.println(reverseNumber);
-		if (givenNumber==reverseNumber)
-		{
-			System.out.println("Pallindrome");
-		}
-		else
-		{
-			System.out.println("N0t Pallindrome");
-		}
+//		while(givenNumber!=0)
+//		{
+//			int reminder=givenNumber%10;
+//			reverseNumber=reverseNumber*10+reminder;
+//			givenNumber=givenNumber/10;
+//		}
+//		System.out.println(reverseNumber);
+//		if (givenNumber==reverseNumber)
+//		{
+//			System.out.println("Pallindrome");
+//		}
+//		else
+//		{
+//			System.out.println("N0t Pallindrome");
+//		}
 	
+		// Reverse a number
 		
+		StringBuffer sb=new StringBuffer(String.valueOf(givenNumber));
+		StringBuffer rev = sb.reverse();
+		System.out.println(rev);
 		
 	}
 
